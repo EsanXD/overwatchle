d3a6859ff2ed643f3f6cb8e9a047dfd72979377b
EsanXD <tran.ethann@gmail.com>
2024-04-13 15:22:37 -0500

    submit character select done

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -10,9 +10,11 @@ import {
   Box,
   Card,
   CardBody,
+  useToast,
+  useMediaQuery,
 } from "@chakra-ui/react";
-import { CloseIcon, HamburgerIcon } from "@chakra-ui/icons";
-import { abilities, styles } from "../util/consts";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import { styles } from "../util/consts";
 import { HeroSelect } from "../heroSelect/HeroSelect";
 import { useEffect, useState } from "react";
 import { Character, DailyWord } from "../util/interfaces";
@@ -36,12 +38,17 @@ export const App = ({
   endless: boolean;
   back: any;
 }) => {
+  const orange = "#FFA301";
+  const grey = "#2D4248";
+  const blue = "#218ffe";
   const [settingsOpen, setSettingsOpen] = useState<boolean>(false);
   const [modalActive, setModalActive] = useState<any>(ModalStates.TUTORIAL);
   const [selectedCharcter, setCharacter] = useState("");
   const [guesses, setGuesses] = useState<Character[]>([]);
   const [currentCharacter, setCurrentCharacter] = useState<Character>();
-  // const;
+  const toast = useToast();
+
+  const [isLargerThan1360] = useMediaQuery("(min-width: 1364px)");
 
   const sanitizeText = (text: string) => {
     return text.replace(/[^a-zA-Z]/g, "").toLowerCase();
@@ -51,13 +58,24 @@ export const App = ({
     return Characters.find((c) => sanitizeText(c.name) === sanitizeText(name))!;
   };
 
+  const handleSubmit = () => {
+    if (currentCharacter) {
+      setGuesses([...guesses, currentCharacter]);
+    } else {
+      toast({
+        title: "Please Select a Character",
+        description: "No character has been selected",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+  };
+
   useEffect(() => {
     setCurrentCharacter(getCharacter(selectedCharcter));
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedCharcter]);
-
-  const orange = "#FFA301";
-  const grey = "#2D4248";
 
   return (
     <Flex
@@ -80,17 +98,21 @@ export const App = ({
         icon={<HamburgerIcon />}
       />
 
-      <Flex gap={4}>
-        {data ? (
+      <Flex
+        gap={4}
+        flexDir={isLargerThan1360 ? "row" : "column"}
+        alignItems={isLargerThan1360 ? "" : "center"}
+      >
+        {data.length ? (
           <Scoreboard guesses={guesses} actual={getCharacter(data[0].hero)} />
         ) : (
           <></>
         )}
-        <Card bgColor={grey}>
-          <CardBody p={0}>
-            <Flex>
+        <Card zIndex={1} maxHeight={300} w={isLargerThan1360 ? 350 : "50vw"}>
+          <CardBody p={0} w={isLargerThan1360 ? 350 : "50vw"}>
+            <Flex bgColor={grey}>
               <Image
-                height={150}
+                height={isLargerThan1360 ? 150 : 75}
                 src={
                   currentCharacter?.img ??
                   "https://blz-contentstack-images.akamaized.net/v3/assets/blt2477dcaf4ebd440c/blt451e9e607acad0dc/64a72e2c9d480a8704791cbd/Dive_Into_Enemy_Lines.png?format=webply&quality=90"
@@ -98,14 +120,32 @@ export const App = ({
                 alt="selected character image"
                 borderRadius="lg"
               />
-              <Flex direction={"column"} p={4} gap={4}>
-                <Heading color={orange}>{currentCharacter?.name}</Heading>
-                <Text>{currentCharacter?.name}</Text>
+              <Flex
+                height={isLargerThan1360 ? 150 : 75}
+                justifyContent={"center"}
+                alignItems={"center"}
+                flex={1}
+              >
+                Timer
               </Flex>
+            </Flex>
+            <Flex
+              direction={"column"}
+              p={4}
+              gap={4}
+              bgColor={"rgba(45, 66, 72, .8)"}
+            >
+              <Heading as="em" style={styles.font} color={blue}>
+                {currentCharacter?.name ?? "CHARACTER"}
+              </Heading>
+              <Button onClick={handleSubmit}>
+                {currentCharacter ? "GUESS" : "SELECT"}
+              </Button>
             </Flex>
           </CardBody>
         </Card>
       </Flex>
+
       <Flex
         direction={"row"}
         wrap={"wrap"}
